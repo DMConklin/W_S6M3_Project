74049f588fbe946a8c37240ba589c3a14d07a231
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-08-09 01:59:05 -0500

    created DateSetter component for handling the logic for a dynamic date to append to the url in our API call

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,17 +1,19 @@
 import React, {useState, useEffect} from 'react'
 import axois from "axios"
+import DateSetter from './DateSetter'
 
 function App() {
+  const currentYear = new Date().getFullYear()
+  const currentMonth = new Date().getMonth()
+  const currentDay = new Date().getDate()
   const [data, setData] = useState()
-  const [year, setYear] = useState(new Date().getFullYear())
-  const [month, setMonth] = useState(new Date().getMonth()+1)
-  const [day, setDay] = useState(new Date().getDate())
-  const [date, setDate] = useState(`${year}-${month}-${day}`)
+  const [year, setYear] = useState(currentYear)
+  const [month, setMonth] = useState(currentMonth)
+  const [day, setDay] = useState(currentDay)
+  const [date, setDate] = useState(`${year}-${month.toString().length < 2 ? '0'+(month+1) : (month+1)}-${day.toString().length < 2 ? '0'+day : day}`)
 
   const BASE_URL = "https://api.nasa.gov/planetary/apod"
   const API_KEY = "DEMO_KEY"
-
-  console.log(data)
 
   useEffect(() => {
     axois.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
@@ -20,10 +22,17 @@ function App() {
   },[date])
 
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
+    <DateSetter 
+      year={year}
+      month={month}
+      day={day}
+      currentYear={currentYear}
+      currentMonth={currentMonth}
+      currentDay={currentDay}
+      setYear={setYear}
+      setMonth={setMonth}
+      setDay={setDay}
+      setDate={setDate} />
   )
 }
 
---
+++ frontend/components/DateSetter.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react'
+
+export default function DateSetter(props) {
+	const {year, 
+			setYear, 
+			month, 
+			setMonth, 
+			day, 
+			setDay,
+			setDate
+		} = props
+
+	const [daysInMonth, setDaysInMonth] = useState(new Date(year, month+1, 0).getDate())
+
+	const years = []
+	const months = Array.from(Array(12).keys())
+	const days = Array.from(Array(daysInMonth).keys())
+	
+	for (let i = currentYear; i >= 1995; i--) {
+		years.push(i)
+	}
+
+	const handleYearChange = e => {
+		setYear(Number(e.target.value))
+	}
+
+	const handleMonthChange = e => {
+		setMonth(Number(e.target.value))
+	}
+
+	const handleDayChange = e => {
+		setDay(Number(e.target.value))
+	}
+
+	const setDateHandler = () => {
+		setDate(`${year}-${month.toString().length < 2 ? '0'+(month+1) : (month+1)}-${day.toString().length < 2 ? '0'+day : day}`)
+	}
+
+	useEffect(() => {
+		setDaysInMonth(new Date(year, month, 0).getDate())
+	}, [month,year])
+
+	return (
+		<div>
+			<select name="year" id="year" onChange={handleYearChange}>
+				{years.map((year, idx) => {
+					return <option key={idx} value={year}>{year}</option>
+				})}
+			</select>
+			<select name="month" id="month" onChange={handleMonthChange}>
+				{months.map((month, idx) => {
+					return <option key={idx} value={month}>{month+1}</option>
+				})}
+			</select>
+			<select name="day" id="day" onChange={handleDayChange}>
+				{days.map((day, idx) => {
+					return <option key={idx} value={day+1}>{day+1}</option>
+				})}
+			</select>
+			<button onClick={setDateHandler}>Get</button>
+		</div>
+	)
+}

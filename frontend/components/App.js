5a0a92770fdbf8bc0634fd04f231ce2151c266f9
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-08-08 20:54:54 -0500

    set up state for a dynamic state, a useEffect that makes an api call when 'date' state updates and a couple variables to hold the base API url and API key

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axois from "axios"
 
 function App() {
+  const [data, setData] = useState()
+  const [year, setYear] = useState(new Date().getFullYear())
+  const [month, setMonth] = useState(new Date().getMonth()+1)
+  const [day, setDay] = useState(new Date().getDate())
+  const [date, setDate] = useState(`${year}-${month}-${day}`)
+
+  const BASE_URL = "https://api.nasa.gov/planetary/apod"
+  const API_KEY = "DEMO_KEY"
+
+  console.log(data)
+
+  useEffect(() => {
+    axois.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
+      .then(res => setData(res.data))
+      .catch(err => console.log(err))
+  },[date])
+
   return (
     <p>
       Read through the instructions in the README.md file to build your NASA
